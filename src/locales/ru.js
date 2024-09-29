17904a2ec05ffc1ac8c1b7828723fd544e9a824e
Max Moon <poriadoc@yandex.ru>
2024-09-29 18:11:20 +0300

    fix locales

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -5,6 +5,7 @@ export default {
       notValidUrl: 'Ссылка должна быть валидным URL',
       someError: 'Упс...',
       notRssSource: 'Ресурс не содержит валидный RSS',
+      'Network Error': 'Ошибка сети',
     },
     success: {
       validUrl: 'RSS успешно загружен',
